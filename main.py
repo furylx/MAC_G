99cd98562a1aff5c66d505d7fae68425e13faa24
furylx <101798731+furylx@users.noreply.github.com>
2023-03-05 20:50:02 +0100

    completely overhauled with argparse, replaced ifconfig with the ip command

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-import json
+from argparse import ArgumentParser
 import subprocess
-import optparse
+import json
 import re
-import os.path
+import random
 
 art = '''
 
@@ -32,95 +31,123 @@ art = '''
 
 '''
 
-
-def get_args():
-    pass
-
-
-def file_check():
-    os.path.exists('./oldmac.json')
+# TODO WITH THE IP COMMAND MAC MUST HAVE :, - IS NOT VALID!
+out_json = subprocess.check_output(['ip', '-j', 'a'])
+out = json.loads(out_json.decode())
 
 
 def get_interfaces():
-    out = subprocess.check_output(['ifconfig', '-s'])
-    out_list = {i + 1: line.split()[0] for i, line in enumerate(out.decode().split('\n')[1:-1])}
-    return out_list
+    return [dic['ifname'] for dic in out if 'address' in dic.keys()]
 
 
 def get_mac(interface):
-    out = subprocess.check_output(['ifconfig', interface])
-    if out:
-        out_list = [line.strip() for line in out.decode().split('\n')[1:] if line.strip()[:5] == 'ether']
-        ether = ''.join(out_list)
-        try:
-            mac_address = (
-                re.search(r'([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2}|([0-9A-Fa-f]{2}[-]){5}[0-9A-Fa-f]{2}', ether)).group(0)
-            if mac_address:
-                return mac_address
-        except:
-            print(f"Could not find a mac address on \'{interface}\'")
+    mac = [dic['address'] for dic in out if interface in dic.values()]
+    return ''.join(mac)
+
+
+def change_mac(interface, mac):
+    try:
+        subprocess.call(['ip', 'link', 'set', 'dev', interface, 'down'])
+        subprocess.call(['ip', 'link', 'set', 'dev', interface, 'address', mac])
+        subprocess.call(['ip', 'link', 'set', 'dev', interface, 'up'])
+        print(f"MAC address has been changed to {mac}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
 
 
 def valid_mac(mac):
-    mac_address = (
-        re.search(r'([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2}|([0-9A-Fa-f]{2}[-]){5}[0-9A-Fa-f]{2}', mac))  # .group(0)
+    print(f"Mac inside valid mac >{mac}<")
+    mac_address = (re.search(r'([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2}', mac))# check if the mac has a valid format (not checking UAA,LAA and multi- or unicast)
     if mac_address is None:
         return False
     else:
         return True
 
 
-def change_mac(interface, mac):
-    if not valid_mac(mac):
-        print(f"{mac} is not a valid mac address!")
-    elif get_mac(interface) == mac.replace('-', ':'):
-        print(f"{interface} already has the mac {mac}")
+def mac_generator(user_uaa=False, user_multicast=False):
+    '''I hope this is alright xD As far as I understand it is and works.'''
+
+    first_byte = random.randint(0x00, 0xff)
+
+    # Set the administration type (UAA or LAA) bit (second least significant bit)
+    if user_uaa:
+        first_byte = first_byte & ~(1 << 1)  # set the bit to 0 // first_byte AND NOT(1 LSHIFT 1)
     else:
-        mac = mac.replace('-', ':')
-        with open('oldmac.json', 'w') as f:
-            json.dump(get_mac(interface), f, indent=4)
-        print(f"Changing mac address on {interface} to {mac}")
-        subprocess.call(['ifconfig', interface, 'down'])
-        subprocess.call(['ifconfig', interface, 'hw', 'ether', mac])
-        subprocess.call(['ifconfig', interface, 'up'])
+        first_byte = first_byte | (1 << 1)  # set the bit to 1 // first_byte  OR (1 LSHIFT 1)
+
+    # Set the transmission type (uni- or multicast) bit (least significant bit)
+    if user_multicast:
+        first_byte = first_byte | 1  # set the bit to 1 // first_byte OR 1
+    else:
+        first_byte = first_byte & ~1  # set the bit to 0 // first_byte AND NOT 1
+
+    mac_address = [first_byte]
+
+    for i in range(5):
+        mac_address.append(random.randint(0x00, 0xff))
+
+    mac_str = ':'.join('{:02x}'.format(byte) for byte in mac_address)
+    return mac_str
 
 
-def reset_mac(interface):
-    if file_check():
-        with open('oldmac.json', 'r') as f:
-            old_mac = json.load(f)
-        print(f"Reverting mac address on {interface} back to {old_mac}")
-        subprocess.call(['ifconfig', interface, 'down'])
-        subprocess.call(['ifconfig', interface, 'hw', 'ether', old_mac])
-        subprocess.call(['ifconfig', interface, 'up'])
+def mac_generator_vendor(vendor_octets):
+    if len(vendor_octets) == 8 and re.search(r'([0-9A-Fa-f]{2}[:]){2}[0-9A-Fa-f]{2}', vendor_octets) is not None:
+        mac_address = []
+
+        for i in range(3):
+            mac_address.append(random.randint(0x00, 0xff))
+        mac_str = ':'.join('{:02x}'.format(byte) for byte in mac_address)
+
+        return vendor_octets + ':' + mac_str
     else:
-        print(
-            f"\nCould not revert to previous mac because the mac probably has not been changed.\nIf you change your mac, the old mac will be backed up.")
+        print("The format must be \'xx:xx:xx\', the last 3 octets are generated automatically.")
 
 
 def main():
     print(art)
-    interface_dict = ", ".join([f"{key}: {value}" for key, value in get_interfaces().items()])
-    interface_choice = ", ".join([f"{key}" for key in get_interfaces().keys()])
-    print(f"\nAvailable interfaces: {interface_dict}")
-    user_interface_choice = int(input(f"\nChoose the interface {interface_choice}:\n\n"))
-    if str(user_interface_choice) not in str(interface_choice):
-        print(f"{user_interface_choice} was not a valid choice, ciao!")
-        return
-    if get_mac(get_interfaces()[user_interface_choice]) is not None:
-        print(
-            f"\nMac address of {get_interfaces()[user_interface_choice]} is {get_mac(get_interfaces()[user_interface_choice])}")
-        change_or_revert = input(
-            "Enter \'revert\' to revert back to your previous mac address, \'change\' to change it or \'exit\' to stop.\n\n")
-        if change_or_revert.lower() == 'change':
-            new_mac = input("Enter the new mac address you want to use\n\n")
-            change_mac(get_interfaces()[user_interface_choice], new_mac)
-        elif change_or_revert.lower() == 'revert':
-            reset_mac(get_interfaces()[user_interface_choice])
-        elif change_or_revert.lower() == 'exit':
-            print("\nBye!")
+    parser = ArgumentParser()
+    # Optional positional argument for interface
+    parser.add_argument('interface', nargs='?', help='Specify interface')
+
+    # Options for setting MAC address
+    parser.add_argument('-U', action='store_true', help='Set random UAA MAC address')
+    parser.add_argument('-L', action='store_true', help='Set random LAA MAC address')
+    parser.add_argument('-v', metavar='XX:XX:XX', help='Set MAC address using vendor prefix')
+    parser.add_argument('-c', metavar='XX:XX:XX:XX:XX:XX', help='Set MAC address to specified value')
+
+    # Option for showing available interfaces
+    parser.add_argument('--show', action='store_true', help='Show available interfaces with MAC addresses')
+
+    args = parser.parse_args()
+
+    if args.show:
+        print(get_interfaces())
+        print("Because show...")
+
+    else:
+        if not args.interface:
+            print(
+                "Interface argument is required when setting or changing the MAC address. Use -h to see the available options.")
         else:
-            print('You did not enter a valid command!')
+            if args.v and not any([args.L, args.c, args.U]):
+                if mac_generator_vendor(args.v) is None:
+                    pass
+                else:
+                    change_mac(args.interface, mac_generator_vendor(args.v))
+
+            elif args.c and not any([args.L, args.U, args.v]):
+                if valid_mac(args.c):
+                    change_mac(args.interface, valid_mac(args.c))
+                else:
+                    print(f"The mac address you entered is not valid")
+            elif args.U and not any([args.L, args.c, args.v]):
+                change_mac(args.interface, mac_generator(True, False))
+
+            elif args.L and not any([args.U, args.c, args.v]):
+                change_mac(args.interface, mac_generator())
+
+            else:
+                print("Invalid combination of arguments. Use -h to see the available options.")
 
 
 if __name__ == '__main__':
